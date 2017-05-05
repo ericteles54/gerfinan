446a6e87a9e45dc35ec21ac2708299a5b178fd6d
Eric Teles <ericteles54@hotmail.com>
2017-05-05 08:35:41 -0300

    commit inicial

---
+++ src/main/java/br/com/qualidadeintegrada/GerfinanApplication.java
@@ -0,0 +1,12 @@
+package br.com.qualidadeintegrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerfinanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerfinanApplication.class, args);
+	}
+}
